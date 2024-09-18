82110e01340b46a03734d04e637dc14bdf1c0d31
alohasally <peacheeks@gmail.com>
2024-09-18 22:23:53 +0900

    add Global style with Style Components

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const StyledApp = styled.div`
+  background-color: gray;
+  padding: 20px;
+`;
+
+export default function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert("check in")}>Check In</Button>
+        <Button onClick={() => alert("check out")}>Check Out</Button>
+        <Input type="number" placeholder="Number of quests" />
+      </StyledApp>
+    </>
+  );
+}
