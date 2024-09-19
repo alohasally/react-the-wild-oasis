06e112ec839ccea47c880c9e7a82798dfac8dc73
alohasally <peacheeks@gmail.com>
2024-09-19 19:30:58 +0900

    add Styled Component Props + css func

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,7 @@ import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`;
+import Heading from "./ui/Heading";
 
 const StyledApp = styled.div`
   background-color: gray;
@@ -19,9 +14,12 @@ export default function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <H1>The Wild Oasis</H1>
+        <Heading as="h1">The Wild Oasis</Heading>
+
+        <Heading as="h2">Check in and out</Heading>
         <Button onClick={() => alert("check in")}>Check In</Button>
         <Button onClick={() => alert("check out")}>Check Out</Button>
+        <Heading as="h3">Form</Heading>
         <Input type="number" placeholder="Number of quests" />
       </StyledApp>
     </>
