49237f3ed47846d7bffaad1903499acb2d88a4ed
alohasally <peacheeks@gmail.com>
2024-09-19 20:09:24 +0900

    building reusuable styled components

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.div`
-  background-color: gray;
   padding: 20px;
 `;
 
@@ -14,13 +14,28 @@ export default function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading as="h1">The Wild Oasis</Heading>
-
-        <Heading as="h2">Check in and out</Heading>
-        <Button onClick={() => alert("check in")}>Check In</Button>
-        <Button onClick={() => alert("check out")}>Check Out</Button>
-        <Heading as="h3">Form</Heading>
-        <Input type="number" placeholder="Number of quests" />
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button onClick={() => alert("check in")}>Check In</Button>
+              <Button
+                variation="secondary"
+                size="medium"
+                onClick={() => alert("check out")}
+              >
+                Check Out
+              </Button>
+            </div>
+          </Row>
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of quests" />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
