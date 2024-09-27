a44f64024ded456da2541052ee4a5c6ccfde36b9
alohasally <peacheeks@gmail.com>
2024-09-27 22:56:55 +0900

    apiSetting

--- src/services/apiSettings.js
+++ src/services/apiSettings.js
@@ -19,9 +19,9 @@ export async function updateSetting(newSetting) {
     .eq("id", 1)
     .single();
 
-  if (error) {
-    console.error(error);
-    throw new Error("Settings could not be updated");
-  }
+  // if (error) {
+  //   console.error(error);
+  //   throw new Error("Settings could not be updated");
+  // }
   return data;
 }
